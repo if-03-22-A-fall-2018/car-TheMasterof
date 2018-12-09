653671f73a58b70ea4a7d99f25952f720f0b9375
Marcel Plakolb <marcel.plakolb@liwest.at>
2018-12-09 18:58:58 +0100

    test preporation (work in progress)

--- car.cpp
+++ car.cpp
@@ -1,41 +1,34 @@
+/*----------------------------------------------------------
+*				HTBLA-Leonding / Klasse: <your class>
+* ---------------------------------------------------------
+* Exercise Number: 0
+* Title:			car.h
+* Author:			P. Bauer
+* Due Date:		January 9, 2015
+* ----------------------------------------------------------
+* Description:
+* Car abstract data type demo.
+* ----------------------------------------------------------
+*/
 #include "car.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-struct CarImplementation jeep1 = {JEEP, SILVER, 0, 0, 80, false};
-struct CarImplementation jeep2 = {JEEP, BLACK, 0, 0, 80, false};
-struct CarImplementation aixam = {AIXAM, RED, 0, 0, 16, false};
-struct CarImplementation multipla1 = {FIAT_MULTIPLA, GREEN, 0, 0, 65, false};
-struct CarImplementation multipla2 = {FIAT_MULTIPLA, BLUE, 0, 0, 65, false};
-struct CarImplementation multipla3 = {FIAT_MULTIPLA, ORANGE, 0, 0, 65, false};
-void init(){
-
+enum car_type get_type(Car car){
+  enum car_type test;
+  return test;
 }
-Car get_car(CarType car){
-  return 0;
-}
-enum CarType get_type(Car car){
-  return JEEP;
-}
-enum Color get_color(Car car){
-  return RED;
+enum car_color get_color(Car car){
+  enum car_color test;
+  return test;
 }
 double get_fill_level(Car car){
-  double test;
-  return test;
+  return 0.0;
 }
-int get_acceleration_rate(Car car){
-  int test;
-  return test;
+double get_acceleration_rate(Car car){
+  return 0.0;
 }
-void set_acceleration_rate(Car car, double rate){
-
-}
-void accelerate(Car car){
-
-}
-double get_speed(Car car){
-  double test;
-  return test;
+int get_speed(Car car){
+  return 0;
 }
--- car.h
+++ car.h
@@ -12,22 +12,25 @@
 */
 #ifndef ___CAR_H
 #define ___CAR_H
-enum CarType{AIXAM, FIAT_MULTIPLA, JEEP};
-enum Color{RED, GREEN, BLUE, ORANGE, SILVER, BLACK};
 
-typedef struct CarImplementation* Car;
+enum car_type{AIXAM, FIAT_MULTIPLA, JEEP };
+enum car_color{RED, GREEN, SILVER, };
 
-struct CarImplementation{
+struct Carimplementation{
+  enum car_type type;
+  enum car_color color;
+  double fill_level;
+  double acceleration_rate;
+  int speed;
+};
 
-}
+typedef struct Carimplementation* Car;
 
-void init();
-Car get_car(CarType car);
-enum CarType get_type(Car car);
-enum Color get_color(Car car);
+enum  get_car(car_type)
+enum car_type get_type(Car car);
+enum car_color get_color(Car car);
 double get_fill_level(Car car);
-int get_acceleration_rate(Car car);
-void set_acceleration_rate(Car car, double rate);
 void accelerate(Car car);
-double get_speed(Car car);
+double set_acceleration_rate(Car car, double acceleration);
+int get_speed(Car car);
 #endif
